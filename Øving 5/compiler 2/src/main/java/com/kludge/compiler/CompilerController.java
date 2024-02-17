11110ceab36a9586f160e1d3898bf7c3e5c5dac3
Andreas <andreksv@ntnu.no>
2024-02-17 11:49:34 +0100

    Reworked code for exercise 5. In progress

--- Øving 5/compiler 2/src/main/java/com/kludge/compiler/CompilerController.java
+++ Øving 5/compiler 2/src/main/java/com/kludge/compiler/CompilerController.java
@@ -1,42 +1,20 @@
 package com.kludge.compiler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 @RestController
+@RequestMapping("/compiler")
 public class CompilerController {
 
+    @Autowired
+    private CompilerService compilerService;
+
     @PostMapping("/compile")
-    public String compileCode(@RequestBody String code) {
-        try {
-            // Create a temporary file
-            File tempFile = File.createTempFile("temp", ".java");
-            
-            // Write code to the temporary file
-            try (FileWriter writer = new FileWriter(tempFile)) {
-                writer.write(code);
-            }
-
-            // Compile and run the code
-            Process process = Runtime.getRuntime().exec("javac " + tempFile.getAbsolutePath());
-            process.waitFor();
-            process = Runtime.getRuntime().exec("java -cp " + tempFile.getParent() + " " + tempFile.getName());
-
-            // Capture the output
-            String output = IOUtils.toString(process.getInputStream(), Charset.defaultCharset());
-
-            // Delete the temporary file
-            tempFile.delete();
-
-            return output;
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            return "Error occurred: " + e.getMessage();
-        }
+    public String compileAndRunCode(@RequestBody String code) {
+        return compilerService.compileAndRunCode(code);
     }
 }
