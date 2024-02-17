43dee0b603f40e1d90b863b052f015136f17eb0a
Andreas <andreksv@ntnu.no>
2024-02-17 12:26:25 +0100

    Reconfigured filestructure for task

---
+++ Øving 5/client/src/components/CodeInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <textarea v-model="code" rows="10" cols="50"></textarea>
+      <button @click="compileCode">Compile</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        code: "",
+      };
+    },
+    methods: {
+      compileCode() {
+        this.$emit("compile", this.code);
+      },
+    },
+  };
+  </script>
+  
---
+++ Øving 5/client/src/components/ResultDisplay.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h2>Compilation Result:</h2>
+      <pre>{{ result }}</pre>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      result: String,
+    },
+  };
+  </script>
+  
